71d5f42a8a88c4b983f13624d79296a46d855da1
zotil <carlosmartinez87@gmail.com>
2025-02-27 16:53:04 -0300

    remove zappa + docker configs

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 # If env PRODUCTION is 1, then run the production server, otherwise if its 0 run the development server
-if [ "$PRODUCTION" = "1" ]; then
+if [ "$STAGE" = "PRODUCTION" ]; then
   # Run the production server
   # -k daphne
   gunicorn -w 8 \
       -k gevent \
       --worker-connections 1000 \
-      --access-logfile logs/access_log \
-      --error-logfile logs/error_log \
       --bind :80 \
       core.wsgi:application
       
-elif [ "$PRODUCTION" = "0" ]; then
+elif [ "$STAGE" = "DEV" ]; then
   # Run the development server
   python manage.py runserver 0.0.0.0:8000
 fi
